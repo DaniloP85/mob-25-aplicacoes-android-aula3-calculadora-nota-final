b08357bf11f6884dd36a7fbbfdc68c0f962156ef
dpsnqmk <danilopsnts@gmail.com>
2022-08-09 22:44:04 -0300

    add calculo da media

--- app/src/main/java/com/example/calculadoradenota/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradenota/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.calculadoradenota
-
+import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_calcular.setOnClickListener {
+            var nota1 =  input_nota_um.text.toString()
+            var nota2 =  input_nota_dois.text.toString()
+            var nota3 =  input_nota_tres.text.toString()
+
+            val media = ((nota1.toInt() + nota2.toInt() + nota3.toInt())/3)
+
+            if (media >= 7){
+                resultado.visibility = TextView.VISIBLE
+                resultado.text = "apravado"
+            }else{
+                resultado.visibility = TextView.VISIBLE
+                resultado.text = "repravado"
+            }
+        }
+
+        btn_limpar.setOnClickListener {
+            resultado.visibility = TextView.INVISIBLE
+            input_nota_um.setText("")
+            input_nota_dois.setText("")
+            input_nota_tres.setText("")
+        }
     }
 }
